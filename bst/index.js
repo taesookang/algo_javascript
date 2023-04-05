86827845b8a0e067b9eab66582441cdb1843ebec
Taesoo <75271289+taesookang@users.noreply.github.com>
2023-04-05 00:13:14 -0400

    BST: class

--- bst/index.js
+++ bst/index.js
@@ -11,6 +11,38 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data) {
+      this.left ? this.left.insert(data) : (this.left = new Node(data));
+    }
+
+    if (data > this.data) {
+      this.right ? this.right.insert(data) : (this.right = new Node(data));
+    }
+  }
+
+  contains(data) {
+    if (data === this.data) {
+      return this;
+    }
+
+    if (data < this.data && this.left) {
+      return this.left.contains(data);
+    }
+
+    if (data > this.data && this.right) {
+      return this.right.contains(data);
+    }
+
+    return null;
+  }
+}
 
 module.exports = Node;
